be228764e9fbb8f66dceb3e509fe6e7d555ca4b1
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-11-11 16:43:24 -0300

    HTML events

---
+++ eventos-html/index.js
@@ -0,0 +1,27 @@
+function clickMe() {
+    console.log('I was clicked!')
+}
+
+function focusMe() {
+    console.log('I was focused!')
+}
+
+function blurMe() {
+    console.log('I was unfocused!')
+}
+
+function changeMe() {
+    console.log('I was changed!')
+}
+
+function overMe() {
+    console.log('Mouse here')
+}
+
+function keydown() {
+    console.log('Something was typed!')
+}
+
+function doubleClick() {
+    console.log('WOW, DOUBLE CLICK!')
+}
